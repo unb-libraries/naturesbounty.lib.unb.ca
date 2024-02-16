d82b35e9c5d942fe18b69b04c499f8bb22dc009d
Camilo Villamizar <cvillami@unb.ca>
2024-02-16 16:43:26 -0400

    NATURE-20 Restrict user admin routes

---
+++ custom/modules/nature_menu/src/Routing/RouteSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\nature_menu\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events and restrict access to routes.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    // Restrict user admin routes.
+    $deny_routes = [
+      'user.pass',
+      'user.register',
+      'user.reset',
+      'entity.user.edit_form',
+    ];
+
+    // Deny access to non-admins.
+    foreach ($deny_routes as $deny_route) {
+      if ($route = $collection->get($deny_route)) {
+        $route->setRequirement('_permission', 'administer_users');
+      }
+    }
+  }
+
+}
